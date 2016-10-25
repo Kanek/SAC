9088a892c7616c4a941112c85546f5cce6724701
Kanek <dark_shadow921@hotmail.com>
2016-10-25 12:48:06 -0500

    creacion de tabla de categorias y control de interfaz

--- js/administracion-inv.js
+++ js/administracion-inv.js
@@ -1,2 +1,23 @@
+$(document).ready(function(){
+	var opButtons 	= document.querySelectorAll(".opButton");
+	var Categorias 	= [];
+	FrontMannager.setOpButtonsListeners(opButtons);
 
-FrontMannager.setOpButtonsListeners();
+	$('#formNuevaCategoria').submit( function(){
+		var str = '';
+		var inputs = $('#formNuevaCategoria :input[type=text]');
+
+		if ( Categorias.indexOf(inputs[0].value) == -1 ) {
+			Categorias.push(inputs[0].value);
+			str 		= 'Se agrego la categoria correctamente.';
+			var d 		= Date()
+			var date 	= d.split(" ");
+			rstr 	= '<tr><td>'+Categorias[Categorias.length - 1]+'</td><td>'+date[1]+'/'+date[2]+'/'+date[3]+'</td></tr>';
+			FrontMannager.addTableInfo('catTable', rstr);
+		}else{
+			str = 'La categoria ya existe. Ingrese una categoria inexistente.';
+		}
+		alert(str);
+		return false;
+	} );
+});
--- js/front-manager.js
+++ js/front-manager.js
@@ -1,25 +1,43 @@
 var FrontMannager = {
 	opButtons: [],
 	flagHolder: '',
-	setOpButtonsListeners: function(){
-		opButtons = document.querySelectorAll(".opButton");
-		console.log(document.querySelectorAll(".opButton .selectedButton"));
+	firstValue: true,
+	setOpButtonsListeners: function(opButtons){
 		for (var i = opButtons.length - 1; i >= 0; i--) {
-			var sectionResp = document.getElementById(opButtons[i] + "Block")
-			this.flagHolder = opButtons[i].flag ? opButtons[i].id : '';
-			opButtons[i].addEventListener("click", function(sectionResp){
-				var flagHolder = FrontMannager.flagHolder;
-				console.log("So far so good...");
-				this.flag = "1";
-				document.getElementById(flagHolder).flag = '0';
-				var oldHolder = document.getElementById(flagHolder + 'Block');
-				oldHolder.className += ' hidden'
-				var newHolder = document.getElementById(this.id + 'Block');
-				var newHolderClass = newHolder.className.split(" ");
-				newHolder.className = newHolderClass[0];
-				console.log(FrontMannager.flagHolder);
+			var button 		= document.getElementById(opButtons[i].id);
+			console.log(button.dataset.flag);
+			FrontMannager.flagHolder = button.dataset.flag ? button.id : '';
+
+			button.addEventListener("click", function(){
+				var newHolder = document.getElementById(this.id+"Block");
+				var oldHolder = document.getElementById(FrontMannager.flagHolder+"Block");
+				var oldButton = document.getElementById(FrontMannager.flagHolder);
+				FrontMannager.flagHolder = this.id;
+
+				FrontMannager.setOpButtonValues(this, oldButton);
+				FrontMannager.setSectionValues(newHolder, oldHolder);
 			});
-		};
+		}
 	},
+
+	setOpButtonValues: function(newB, oldB){
+		newB.className	+=' selectedButton';
+		var oldClass 	=oldB.className.split(" ");
+		oldB.className 	= oldClass[0];
+	},
+
+	setSectionValues: function(newH, oldH){
+		oldH.className	+=' hidden';
+		var newClass 	= newH.className.split(" ");
+		newH.className	= newClass[0];
+	},
+
+	addTableInfo: function(id, val){
+		var table = document.getElementById(id);
+		if (table.className == "hidden") {
+			table.className = '';
+		}
+		table.innerHTML+=val;
+	}
 
 }
